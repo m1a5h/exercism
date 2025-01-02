ec3d48a86a4af1f6547c0fae20094fb10c6f2b65
Matthew Ashman <matthewiashman@gmail.com>
2025-01-02 21:41:21 +0000

    Usef something format can do to remove a line from verse function

--- common-lisp/beer-song/beer-song.lisp
+++ common-lisp/beer-song/beer-song.lisp
@@ -8,8 +8,7 @@
   (case n
     (0 (format nil "No more bottles of beer on the wall, no more bottles of beer.~%Go to the store and buy some more, 99 bottles of beer on the wall.~%"))
     (1 (format nil "~d bottle of beer on the wall, ~d bottle of beer.~%Take it down and pass it around, no more bottles of beer on the wall.~%" n n))
-    (2 (format nil "~d bottles of beer on the wall, ~d bottles of beer.~%Take one down and pass it around, ~d bottle of beer on the wall.~%" n n (1- n)))
-  (otherwise (format nil "~d bottles of beer on the wall, ~d bottles of beer.~%Take one down and pass it around, ~d bottles of beer on the wall.~%" n n (1- n)))))
+  (otherwise (format nil "~d bottles of beer on the wall, ~d bottles of beer.~%Take one down and pass it around, ~d bottle~:p of beer on the wall.~%" n n (1- n)))))
 
 (defun sing (start &optional (end 0))
   "Returns a string of verses for a given range of numbers."
